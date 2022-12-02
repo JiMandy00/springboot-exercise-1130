21f60f91de7d03a22d8be8f7c6088f700d4c9ae9
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-12-02 13:28:05 +0900

    2022 11 30 - security

---
+++ src/main/java/com/exercise/exercise1130/configuration/EncoderConfig.java
@@ -0,0 +1,17 @@
+package com.exercise.exercise1130.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class EncoderConfig {
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
